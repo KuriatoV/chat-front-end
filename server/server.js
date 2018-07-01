36e52c0542556a8de3ef40c0bcb14a2c7d6966b5
KuriatoV <kuriato.valentyn@gmail.com>
2018-07-01 16:11:25 +0300

    s

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const http = require('http');
 
 const { generateMessage, generateLocationMessage } = require('./utils/message');
 const socketIO = require('socket.io');
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '/dist/');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -13,7 +13,8 @@ const io = socketIO(server);
 
 app.use(express.static(publicPath));
 app.get(/.*/, function(req, res) {
-	res.sendFile(__dirname + '/dist/index.html');
+	res.send('lol');
+	// res.sendFile(__dirname + '/dist/index.html');
 });
 io.on('connection', (socket) => {
 	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
