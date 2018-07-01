5c5d460d89227e355e2fb752eb9eeff1435788e5
KuriatoV <kuriato.valentyn@gmail.com>
2018-07-01 17:15:54 +0300

    save

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueSocketio from 'vue-socket.io';
 
-Vue.config.productionTip = false
+const socketServer = 'http://localhost:3000';
+Vue.use(VueSocketio, socketServer);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+	render: (h) => h(App)
+}).$mount('#app');
